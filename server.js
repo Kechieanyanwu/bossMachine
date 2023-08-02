d0b58e412533d3d549835941bb3b56c545cf55cb
Nkechi Anyanwu <kechie,94@gmail.com>
2023-08-02 10:48:55 +0100

    Include API routes in pseudocode

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 module.exports = app;
 
@@ -8,18 +11,25 @@ module.exports = app;
 */
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
+//Nkechi: Add morgan for logging HTTP requests to console
+app.use(morgan("tiny"));
 
+// Add middleware for handling CORS requests from index.html
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use("/api", apiRouter);
 
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
+  app.listen(PORT, ()=> {
+    console.log('Server is listening on ' + PORT);
+})
+};
 
-}
