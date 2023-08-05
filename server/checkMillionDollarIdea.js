a248ae1e5a9b3db735ba9df95b4a5410017f0718
Nkechi Anyanwu <122885336+Kechieanyanwu@users.noreply.github.com>
2023-08-05 16:17:56 +0100

    Update checkMillionDollarIdea.js
    Clean up commentary

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -20,28 +20,3 @@ const checkMillionDollarIdea = (req, res, next) => {
 
 module.exports = checkMillionDollarIdea;
 
-// 13) is a function takes three arguments
-// 14) sends a 400 error if the total yield is less than one million dollars
-// 15) calls next for ideas that will yield at least one million dollars
-// 16) sends a 400 error if numWeeks or weeklyRevenue is not supplied
-// 17) sends a 400 error if numWeeks or weeklyRevenue is an invalid string
-
-
-
-
-//refactor
-
-
-  
-
-// const checkMillionDollarIdea = (req, res, next) => {
-//     const idea = req.body;
-//     if ((idea.numWeeks * idea.weeklyRevenue) >= 1000000) {
-//         req.idea = idea;
-//         next(); 
-//     } else {
-//         const err = new Error("Must be worth atleast a milli");
-//         err.status = 400;
-//         next(err);
-//     }
-// }
